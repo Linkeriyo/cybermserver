a071d98654362515823c27b417e0961b65953037
Linkeriyo <linkeriyo@gmail.com>
2021-05-04 18:45:29 +0200

    Update models.py

--- users/models.py
+++ users/models.py
@@ -22,11 +22,34 @@ class UserExtraData(models.Model):
     province = models.CharField(max_length=30)
     zip_code = models.CharField(max_length=7)
 
+    def json(self):
+        json = {
+            'pk': user.pk,
+            'email': self.email,
+            'name': self.name,
+            'surname': self.surname,
+            'telephone_number': self.telephone_number,
+            'address': self.address,
+            'city': self.city,
+            'province': self.province,
+            'zip_code': self.zip_code
+        }
+        return json
+
 
 class Computer(models.Model):
     mac_address = models.CharField(max_length=12)
     ip_address = models.CharField(max_length=50)
     alias = models.CharField(max_length=50)
+
+    def json(self):
+        json = {
+            'pk': self.pk,
+            'mac_address': self.mac_address,
+            'ip_address': self.ip_address,
+            'alias': self.alias
+        }
+        return json
 
 
 class CyberCafe(models.Model):
@@ -39,14 +62,43 @@ class CyberCafe(models.Model):
     telephone_number = models.CharField(max_length=9)
     email = models.CharField(max_length=50)
 
+    def json(self):
+        json = {
+            'pk': self.pk,
+            'name': self.name,
+            'description': self.description,
+            'address': self.address,
+            'city': self.city,
+            'province': self.province,
+            'zip_code': self.zip_code,
+            'telephone_number': self.telephone_number,
+            'email': self.email
+        }
+
 
 class ProductType(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
+
+    def json(self):
+        json = {
+            'pk': self.pk,
+            'name': self.name,
+            'description': self.description
+        }
+        return json
 
 
 class Product(models.Model):
     product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
     reference = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
-
+    
+    def json(self):
+        json = {
+            'pk': self.pk,
+            'product_type': self.product_type,
+            'reference': self.reference,
+            'description': self.description
+        }
+        return json
