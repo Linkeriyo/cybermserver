854f06efbe74819667babe157f5496744b20c349
Linkeriyo <linkeriyo@gmail.com>
2021-05-30 21:12:54 +0200

    changing ' to " cos yes

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from json.decoder import JSONDecodeError
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.http import JsonResponse
@@ -6,7 +7,7 @@ import json
 
 from django.views.decorators.csrf import csrf_exempt
 
-from users.models import UserExtraData, UserToken
+from users.models import UserCybercafes, UserExtraData, UserToken
 from utilities import Token
 
 @csrf_exempt
@@ -30,6 +31,27 @@ def check_user(token, username):
             return {
                 "result": "error",
                 "message": "token does not match with username"
+            }
+
+        return {
+            "result": "ok",
+            "message": "token is valid"
+        }
+
+    except Exception as e:
+        return {
+            "result": "error",
+            "message": str(e)
+        }
+
+def check_token(token):
+    try:
+        user_token = get_object_or_None(UserToken, token=token)
+
+        if user_token is None:
+            return {
+                "result": "error",
+                "message": "token is null"
             }
 
         return {
@@ -147,10 +169,10 @@ def logout(request):
 @csrf_exempt
 def signup(request):
     try:
-        data = json.loads(request.POST['data'])
-        username = data.get('username')
-        password = data.get('password')
-        email = data.get('email')
+        data = json.loads(request.POST["data"])
+        username = data.get("username")
+        password = data.get("password")
+        email = data.get("email")
 
         # -- user and pswd checking --
         if username is None or username == "":
@@ -189,13 +211,13 @@ def signup(request):
 @csrf_exempt
 def check_user_extra_data(request):
     try:
-        data = json.loads(request.POST['data'])
-        token = data.get('token')
-        username = data.get('username')
+        data = json.loads(request.POST["data"])
+        token = data.get("token")
+        username = data.get("username")
 
         check_result = check_user(token, username)
 
-        if check_result.get('result') != 'ok':
+        if check_result.get("result") != "ok":
             return JsonResponse(check_result)
 
         user_token = get_object_or_None(UserToken, token=token)
@@ -227,19 +249,19 @@ def check_user_extra_data(request):
 @csrf_exempt
 def set_user_extra_data(request):
     try:
-        data = json.loads(request.POST['data'])
-        token = data.get('token')
-        username = data.get('username')
-        name = data.get('name')
-        surname = data.get('surname')
-        phono = data.get('phono')
-        address = data.get('address')
-        city = data.get('city')
-        province = data.get('province')
-        zip_code = data.get('zip_code')
+        data = json.loads(request.POST["data"])
+        token = data.get("token")
+        username = data.get("username")
+        name = data.get("name")
+        surname = data.get("surname")
+        phono = data.get("phono")
+        address = data.get("address")
+        city = data.get("city")
+        province = data.get("province")
+        zip_code = data.get("zip_code")
 
         check_result = check_user(token, username)
-        if check_result.get('result') != 'ok':
+        if check_result.get("result") != "ok":
             return JsonResponse(check_result)
         
         user_token = get_object_or_None(UserToken, token=token)
@@ -274,3 +296,32 @@ def set_user_extra_data(request):
             "traceback": str(e)
         })
     
+@csrf_exempt
+def add_cybercafe_to_user(request):
+    try:
+        data = json.loads(request.POST["data"])
+        token = data.get("token")
+        business = data.get("business")
+
+        usertoken = get_object_or_None(UserToken, token=token)
+        
+        if usertoken is None:
+            return JsonResponse({
+                "result": "error",
+                "message": "token is null"
+            })
+        
+        user_cybercafe = UserCybercafes(user=usertoken.user, business=business)
+        user_cybercafe.save()
+        
+        return JsonResponse({
+            "result": "ok",
+            "message": "user_cybercafe added successfully",
+            "user_cybercafe": user_cybercafe.json()
+        })
+    except Exception as e:
+        return JsonResponse({
+            "result": "error",
+            "message": "something went wrong on the server",
+            "traceback": str(e)
+        })
